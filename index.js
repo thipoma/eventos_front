4e3564cd882f437d54a4d99b70acf5867ed0976f
professorisidro <isidro@professorisidro.com.br>
2020-07-22 12:03:08 -0300

    agora com login

--- index.js
+++ index.js
@@ -9,7 +9,46 @@ function clickBotaoEnviar(){
     var txtEmail = document.getElementById("txtEmail").value;
     var txtSenha = document.getElementById("txtSenha").value;
 
-    console.log("Email: "+txtEmail+" /  Senha: "+txtSenha);
+    //console.log("Email: "+txtEmail+" /  Senha: "+txtSenha);
+
+    // a partir do momento que eu capturo as informações, preciso agora criar uma requisicao 
+    // POST para nosso backEnd
+    // Fazendo o paralelo com o POSTMAN
+    // Defino um URL e um método do tipo POST
+    // Defino um body contendo um JSON (do tipo JSON), que tem um objeto do tipo Usuario
+    // apenas com email e senha
+
+    // corpo da mensagem
+    var msgBody = {   
+        email : txtEmail,
+        senha : txtSenha
+    }
+
+    // agora preciso definir o cabecalho da requisicao
+    var cabecalho = {
+        method : 'POST',
+        body   : JSON.stringify(msgBody),
+        headers : {
+            'Content-type':'application/json'
+        }
+    }
+
+    // chamo a funcao FETCH para entrar em contato com o BackEnd
+    fetch("http://localhost:8088/login", cabecalho)
+        .then(res => tratarResposta(res));
+}
+
+function tratarResposta(objeto){
+    console.log(objeto.status);
+    if (objeto.status === 200){
+        alert("UHUUUUU");
+    }
+    else if (objeto.status === 401){
+        alert("SENHA INVALIDA");
+    }
+    else {
+        alert("USUARIO NAO EXISTE NO SISTEMA");
+    }
 }
 
 function verificaTecla(event){
